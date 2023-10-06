e8a7c9a3e893363e316357063439b423860ea5e3
Sumit Kumar <97524217+Sumit0o0@users.noreply.github.com>
2023-10-06 12:34:50 +0530

    Compelted!

--- middleware/dataPassing.js
+++ middleware/dataPassing.js
@@ -4,25 +4,19 @@ import USERDATA from "../model/UserDataSchema.js";
 const getUserInfo = async (req, res, next) => {
   try {
     let userId = req.session.user_id;
-    let adminId = req.session.admin_id;
     let isUser = await USERREGISTERMODEL.findOne({ _id: userId, role:"user" });
     let isAdmin = await USERREGISTERMODEL.findOne({ _id: userId, role:"admin" });
-    const userval = await USERDATA.findOne({ user: userId });
-    let user = null;
+    let userval = await USERDATA.findOne({ user: userId });
+    let user ;
     let isCart = 0;
-    if (userId) {
-      user = isUser;
+    if(userId || isAdmin){
+      user = isUser || isAdmin;
       isCart = userval.cart.length;
-    } else if (adminId) {
-      user = isAdmin;
-    } else {
-      if (req.session.cart && Array.isArray(req.session.cart)) {
+    }else{
+      if(req.session.cart && Array.isArray(req.session.cart)){
         isCart = req.session.cart.length;
-      } else {
-        isCart = 0;
       }
     }
-
     req.user = user;
     req.cartval = isCart;
     next();
